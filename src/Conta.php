e02b60b6a5132c9de83bdebe952440a09052d629
Henrique523 <guilherme.hlg.eng@gmail.com>
2020-02-06 07:14:44 -0300

    Implementando classes sacar e depositar.

--- src/Conta.php
+++ src/Conta.php
@@ -4,4 +4,20 @@ class Conta {
     public $cpfTitular;
     public $nomeTitular;
     public $saldo;
+
+    public function sacar (float $valorASacar) {
+        if($valorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+        }else{
+            $this->saldo -= $valorASacar;
+        }
+    }
+
+    public function depositar (float $valorADepositar): void {
+        if($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+        }else{
+            $this->saldo += $valorADepositar;
+        }
+    }
 }
