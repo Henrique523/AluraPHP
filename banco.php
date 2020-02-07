9d9d9afc84424a0cb490e7c581c3f125211d8bdf
Henrique <suporte@pcmsolucoes.com.br>
2020-02-07 07:47:29 -0300

    Enviando parâmetros na criação de nova conta.

--- banco.php
+++ banco.php
@@ -2,14 +2,10 @@
 
 require_once 'src/Conta.php';
 
-$primeiraConta = new Conta();
+$primeiraConta = new Conta('123.456.789-10', 'Henrique');
 $primeiraConta->depositar(500);
 $primeiraConta->sacar(300);
 
-$primeiraConta->defineCpfTitular('123.456.789-10');
-$primeiaConta->defineNomeTitular('Henrique');
-
-
-echo $primeiraConta->recuperaSaldo();
-echo $primeiraConta->recuperaCpfTitular();
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaNomeTitular();
