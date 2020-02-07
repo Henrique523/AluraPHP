b054c9e6f79df26fb482d6cfcf3d1734a6ffad80
Henrique <suporte@pcmsolucoes.com.br>
2020-02-07 07:35:16 -0300

    Implementação da classe Conta.php no código.

--- banco.php
+++ banco.php
@@ -4,4 +4,12 @@ require_once 'src/Conta.php';
 
 $primeiraConta = new Conta();
 $primeiraConta->depositar(500);
-$primeiraConta->sacar(300);  //Isso é ok
+$primeiraConta->sacar(300);
+
+$primeiraConta->defineCpfTitular('123.456.789-10');
+$primeiaConta->defineNomeTitular('Henrique');
+
+
+echo $primeiraConta->recuperaSaldo();
+echo $primeiraConta->recuperaCpfTitular();
+echo $primeiraConta->recuperaNomeTitular();
